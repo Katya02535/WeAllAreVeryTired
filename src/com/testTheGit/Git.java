d2eae80215c5fe4132c3a524809aa8147f5d838b
Katya02535 <melkotrva83@gmail.com>
2021-03-11 20:29:34 -0500

    new file added

---
+++ src/com/testTheGit/Git.java
@@ -0,0 +1,10 @@
+package com.testTheGit;
+
+public class Git {
+	
+	public static void main (String [] args) {
+		
+		System.out.println("Katya you are doing your best! Keep up the jood work!");
+	}
+
+}
